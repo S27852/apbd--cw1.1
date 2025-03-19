c6a0e4d612030399638062b21c74e7acdd257f73
Krzysztof Kukowski <s27852@pjwstk.edu.pl>
2025-03-19 17:00:22 +0100

    Kod dodalem

--- Cw1.1/Cw1.1/Program.cs
+++ Cw1.1/Cw1.1/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Cw1._1;
+
 Console.WriteLine("Hello, World!");
+
+Student student = new Student();
+student.setName("jarek");
+
+
+Console.WriteLine(student.getName());
---
+++ Cw1.1/Cw1.1/Student.cs
@@ -0,0 +1,18 @@
+namespace Cw1._1;
+
+public class Student
+{
+    private string name;
+    private string surname;
+    private int age;
+
+    public string getName()
+    {
+        return name;
+    }
+
+    public void setName(string _name)
+    {
+        name = _name;
+    }
+}
